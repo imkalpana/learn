72a4b6b2670d37b4c200e2862ce030f94b964c02
Kalpana <158153389+imkalpana@users.noreply.github.com>
2025-09-13 14:56:28 +0000

    Made stack and perform tasks
    Append fruits' names in stack
    print whole stack
    remove items from stack
    again print stack

--- main.py
+++ main.py
@@ -1,8 +1,47 @@
+
+#Made a function which add two number 
+
+print("==== Add two numbers ====")
+print("")
+
 def add(x=3,y=5):
     sum = x+y
     print("Sum of two numbers: ", sum)
 
-add()
-add(121, 234)
-add(1324,42346)
-add(73, 64)
+
+print("add default values: ", add())
+print("add 121 and 234: ",add(121, 234))
+print("add 1324 and 42346: ",add(1324,42346))
+print("add 73 and 64: ",add(73, 64))
+
+#Made a stack 
+
+print("")
+print("==== Stack ====")
+print("")
+stack=[]
+
+print("Stack: ",stack)
+
+#Add fruits' name in the stack
+
+stack.append("Apple")
+stack.append("Banana")
+stack.append("Orange")
+stack.append("Stawberry")
+stack.append("Blueberry")
+
+print("Stack: ",stack)
+
+#Remove an items one by one from stack
+
+pop_item=stack.pop()
+print("Stack: ",stack)
+print("Popped item: ", pop_item)
+
+stack.pop()
+print("Stack: ",stack)
+
+
+
+
